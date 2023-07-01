845b436200c7c79f6728f460d7febfc3cba9b0dc
PolliMi00 <giandomenicopanettieri@gmail.com>
2023-07-01 15:47:05 +0200

    nmos_texture

--- r_full_test.py
+++ r_full_test.py
@@ -14,8 +14,7 @@ class Myscene(Scene):
                y0=1, size=2,value_text="3k",value_font_scale=0.7,
                value_up_shift_offset=0)
         R1_img=R1.get_group()
-
-        L1 = L(name="R1",value_text="17.4n",x0=3,y0=1,
+'''        L1 = L(name="R1",value_text="17.4n",x0=3,y0=1,
                size=2,value_font_scale=0.8,value_right_shift_offset=0.4)
         L1_img = L1.get_group()
 
@@ -24,7 +23,7 @@ class Myscene(Scene):
 
         R1L1C1=Group(R1_img,L1_img,C1_img)
         self.play(FadeIn(R1L1C1))
-        self.wait(3)
+        self.wait(3)'''
 
 
 

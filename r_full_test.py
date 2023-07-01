720a8603c491755535011f39a6141c6a9a25d82e
PolliMi00 <giandomenicopanettieri@gmail.com>
2023-07-01 19:47:01 +0200

    pp

--- r_full_test.py
+++ r_full_test.py
@@ -3,7 +3,6 @@ from NetList import *
 from components import *
 from utils import *
 
-#
 
 class Myscene(Scene): 
 
@@ -15,32 +14,33 @@ class Myscene(Scene):
         Rd = R( name = "Rd", value_text="3k",
                 x0=0,y0=2, size=1)
         Rd_img = Rd.get_group()
-        self.add(Rd_img)
+
+        
+        Rs = R( name = "Rs", value_text="2k",
+                x0=0,y0=-1.5, size=1)
+        Rs_img = Rs.get_group()
+
+        Cs = C( name = "Cs", value_text = "1n",
+                x0=1,y0=-1.5, size= 1
+               )
+        Cs_img = Cs.get_group()
 
         M1 = NMOS(name = "M1", x0=-0.5 , y0=0, size=1)
         M1_img = M1.get_group()
-        self.add(M1_img)
+
+        Rs = R( name = "Rs", value_text="3k",
+                x0=0,y0=2, size=1)
+        Rs_img = Rs.get_group()
+
+        GND1 = GND(" ",0,-3,1)
+        GND1_img = GND1.get_group()
+
+        self.play(M1_img.invert)
+
+        self.wait(2)
 
         
-        net_list = Net()
-
-        line_list = [
-            connect_terminal(M1.nd,Rd.n1),
-            connect_terminal([0,1,0],[-1,1,0])
-        ]
-        sub_net_1 = Net(line_list)
         
-        line_list = connect_nodes_oriented(
-            [[0,1,0],
-            [1,1,0]]
-        )
-        sub_net_2 = Net(line_list)
-
-        net_list.merge_subnet(sub_net_1)
-        net_list.merge_subnet(sub_net_2)
-
-        net_list_img=net_list.net_mobject()
-        self.add(net_list_img)
 
 
 
